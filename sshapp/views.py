382788cc5dc8115c91c3b290e9e489d23e99e7d1
Ernst Haagsman <ernst.haagsman@jetbrains.com>
2016-11-04 16:29:05 +0100

    Views, part 3 of tutorial

--- sshapp/views.py
+++ sshapp/views.py
@@ -1,8 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 
 # Create your views here.
+from sshapp.models import Question
+
 
 def index(request):
-    return HttpResponse("<p>Hello world! Polls index!</p>")
+    latest_questions = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_questions': latest_questions
+    }
+    return render(request, 'sshapp/index.html', context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'sshapp/detail.html', {
+        'question': question
+    })
+
+
+def results(request, question_id):
+    format_string = "You're looking at the results of question %s"
+    response = str.format(format_string, question_id)
+    return HttpResponse(response)
+
+
+def vote(request, question_id):
+    format_string = "You're voting on question %s"
+    response = str.format(format_string, question_id)
+    return HttpResponse(response)
