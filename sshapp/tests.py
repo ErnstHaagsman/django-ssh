b395149bdf905b7ef391b6d84b6ad276175a9960
Ernst Haagsman <ernst.haagsman@jetbrains.com>
2016-11-04 17:39:15 +0100

    Add test for was_published_recently

--- sshapp/tests.py
+++ sshapp/tests.py
@@ -1,3 +1,19 @@
+import datetime
+
 from django.test import TestCase
 
 # Create your tests here.
+from django.utils import timezone
+
+from sshapp.models import Question
+
+
+class QuestionMethodTests(TestCase):
+    def test_published_recently_with_future_question(self):
+        """
+        was_published_recently should return False for questions
+        with a pub_date in the future
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
